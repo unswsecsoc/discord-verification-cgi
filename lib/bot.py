3386073003e04dd103e9561c7f238517b2cb3908
tom <tom@tomn.me>
2021-03-14 18:16:42 +1100

    add stuff to gitignore

--- lib/bot.py
+++ lib/bot.py
@@ -1,14 +1,17 @@
 import requests
 import config
+import sys
 
 BASE_URL = 'https://discord.com/api'
 
 session = requests.Session()
 session.headers.update({'authorization': f'Bot {config.DISCORD_BOT_TOKEN}'})
 
+
 def add_roles(user_id, roles):
   url = f'{BASE_URL}/guilds/{config.DISCORD_GUILD}/members/{user_id}/roles'
   for role in roles:
     resp = session.put(f'{url}/{role}')
     if resp.status_code != 204:
-      raise 
+      print("api error:", resp.text, file=sys.stdout)
+      raise Exception
